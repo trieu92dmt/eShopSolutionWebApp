bf25a0fcf505b1c2471606fea86d44c23275741f
trieu92dmt <trieu251101@gmail.com>
2022-09-09 09:54:38 +0700

    Add project files.

---
+++ eShopSolutionWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eShopSolutionWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
