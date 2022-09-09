efb115db0112805ef696e6838fa8aed4999b7b6d
trieu92dmt <trieu251101@gmail.com>
2022-09-09 13:47:03 +0700

    migration database

---
+++ eShopSolution.Data/EF/EShopDBContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    class EShopDBContextFactory : IDesignTimeDbContextFactory<EShopDBContext>
+    {
+        public EShopDBContext CreateDbContext(string[] args)
+        {
+            //IConfigurationRoot configuration = new ConfigurationBuilder()
+            //    .SetBasePath(Directory.GetCurrentDirectory())
+            //    .AddJsonFile("appsettings.json")
+            //    .Build();
+
+            //var connectionString = configuration.GetConnectionString("eShopSolutionDb");
+
+            var optionsBuilder = new DbContextOptionsBuilder<EShopDBContext>();
+            //optionsBuilder.UseSqlServer(connectionString);
+            optionsBuilder.UseSqlServer(@"Server=MINHTRIEU\SQLEXPRESS01;Database=eShopSolution;Trusted_Connection=True;");
+
+            return new EShopDBContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ eShopSolution.Data/Migrations/20220909061049_Initial.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eShopSolution.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppConfigs",
+                columns: table => new
+                {
+                    Key = table.Column<string>(nullable: false),
+                    Value = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppConfigs", x => x.Key);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SortOrder = table.Column<int>(nullable: false),
+                    IsShowOnHome = table.Column<bool>(nullable: false),
+                    ParentId = table.Column<int>(nullable: true),
+                    Status = table.Column<int>(nullable: false, defaultValue: 1)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Price = table.Column<decimal>(nullable: false),
+                    OriginalPrice = table.Column<decimal>(nullable: false),
+                    Stock = table.Column<int>(nullable: false, defaultValue: 0),
+                    ViewCount = table.Column<int>(nullable: false, defaultValue: 0),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    SeoAlias = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductInCategories",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(nullable: false),
+                    CategoryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductInCategories", x => new { x.CategoryId, x.ProductId });
+                    table.ForeignKey(
+                        name: "FK_ProductInCategories_Products_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductInCategories_Categories_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductInCategories_ProductId",
+                table: "ProductInCategories",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppConfigs");
+
+            migrationBuilder.DropTable(
+                name: "ProductInCategories");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
